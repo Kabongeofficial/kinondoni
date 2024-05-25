4ea979dd69054987f6705e2b0e53551049933a61
kabongeofficial <musakabonge01@gmail.com>
2024-05-25 22:53:09 +0300

    first commit of the project

--- contact.php
+++ contact.php
@@ -3,8 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Dakimp International Company Limited</title>
-    
+    <title>Kinondoni Hospital</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
@@ -35,7 +34,7 @@
 <body>
     <!-- Spinner Start -->
     <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+        <div class="spinner-grow text-success" style="width: 3rem; height: 3rem;" role="status">
             <span class="sr-only">Loading...</span>
         </div>
     </div>
@@ -43,16 +42,16 @@
 
 
     <!-- Navbar Start -->
-    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow border-top border-5 border-primary sticky-top p-0">
-        <a href="index.html" class="navbar-brand bg-primary d-flex align-items-center px-4 px-lg-5">
-            <h2 class="mb-2 text-white">Dakimp International Company</h2>
+    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow border-top border-5 border-success sticky-top p-0">
+        <a href="index.html" class="navbar-brand bg-success d-flex align-items-center px-4 px-lg-5">
+            <h2 class="mb-2 text-white"> Kinondoni Hospital</h2>
         </a>
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index.html" class="nav-item nav-link">Home</a>
+                <a href="index.html" class="nav-item nav-link active">Home</a>
                 <a href="about.html" class="nav-item nav-link">About</a>
                 <a href="service.html" class="nav-item nav-link">Services</a>
                 <div class="nav-item dropdown">
@@ -60,11 +59,13 @@
                     <div class="dropdown-menu fade-up m-0">
                         <a href="feature.html" class="dropdown-item">Features</a>
                         <a href="team.html" class="dropdown-item">Our Team</a>
+                   
+                      
                     </div>
                 </div>
-                <a href="contact.php" class="nav-item nav-link active">Contact</a>
+                <a href="contact.php" class="nav-item nav-link">Contact</a>
             </div>
-            <h4 class="m-0 pe-lg-5 d-none d-lg-block"><i class="fa fa-headphones text-primary me-3"></i>+255 757789208</h4>
+            <h4 class="m-0 pe-lg-5 d-none d-lg-block"><i class="fa fa-phone text-success me-3"></i>+255 677 122 574</h4>
         </div>
     </nav>
     <!-- Navbar End -->
@@ -150,15 +151,15 @@
     
 
 
-    <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-light footer pt-5 wow fadeIn" data-wow-delay="0.1s" style="margin-top: 6rem;">
+     <!-- Footer Start -->
+     <div class="container-fluid bg-dark text-light footer pt-5 wow fadeIn" data-wow-delay="0.1s" style="margin-top: 6rem;">
         <div class="container py-5">
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-light mb-4">Address</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Haidery Plaza, Kisutu Street, Dar es Salaam, Tanzania</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+255 757 789 208</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@dakimpinternational.co.tz</p>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Kawawa Road, Near Kinondoni B Bus stand Dar Es Salaam, Tanzania </p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+255 677 122 574</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@kinondonihospital.co.tz</p>
                     <div class="d-flex pt-2">
                         <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
@@ -167,27 +168,27 @@
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6">
-                    <h4 class="text-light mb-4">Services</h4>
-                    <a class="btn btn-link" href="">Air Freight</a>
-                    <a class="btn btn-link" href="">Sea Freight</a>
-                    <a class="btn btn-link" href="">Road Freight</a>
-                    <a class="btn btn-link" href="">Logistic Solutions</a>
-                    <a class="btn btn-link" href="">Industry solutions</a>
+                    <h4 class="text-light mb-4">Our Collaboratoes</h4>
+                    <a class="btn btn-link" href="">NHIF</a>
+                    <a class="btn btn-link" href="">NSSF</a>
+                    <a class="btn btn-link" href="">STRATEGIES</a>
+                    <a class="btn btn-link" href="">ASSEMBLE INSURANCE</a>
+                    <a class="btn btn-link" href="">BRITAM INSURANCE</a>
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-light mb-4">Quick Links</h4>
-                    <a class="btn btn-link" href="">About Us</a>
-                    <a class="btn btn-link" href="">Contact Us</a>
+                    <a class="btn btn-link" href="">Wizara ya Afya</a>
+                    <a class="btn btn-link" href="">Muhimbili Hospital</a>
                     <a class="btn btn-link" href="">Our Services</a>
                     <a class="btn btn-link" href="">Terms & Condition</a>
                     <a class="btn btn-link" href="">Support</a>
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-light mb-4">Newsletter</h4>
-                    <p>Designed to keep you informed about the latest news, updates, and offers.</p>
+                    <p>Designed to keep you informed about the latest news, updates, and offers available at Kinondoni Hospital.</p>
                     <div class="position-relative mx-auto" style="max-width: 400px;">
                         <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-                        <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+                        <button type="button" class="btn btn-success py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
                     </div>
                 </div>
             </div>
@@ -196,11 +197,11 @@
             <div class="copyright">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a class="border-bottom" href="#">Dakimp Int</a>, All Right Reserved.
+                        &copy; <a class="border-bottom" href="#">Kinondoni Hospital</a>, All Right Reserved.
                     </div>
                     <div class="col-md-6 text-center text-md-end">
                         <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                        Designed By <a class="border-bottom" href="https://htmlcodex.com">Emmanuel</a>
+                        Designed By <a class="border-bottom" href="https://htmlcodex.com">Kabonge</a>
                     </div>
                 </div>
             </div>
@@ -210,7 +211,7 @@
 
 
     <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-0 back-to-top"><i class="bi bi-arrow-up"></i></a>
+    <a href="#" class="btn btn-lg btn-success btn-lg-square rounded-0 back-to-top"><i class="bi bi-arrow-up"></i></a>
 
 
     <!-- JavaScript Libraries -->
